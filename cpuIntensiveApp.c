e3400e9ad2c7a9841cb444657675b81725405ce1
多肉的Siri <SeveirRoy@users.noreply.github.com>
2017-10-09 17:45:04 +0800

    Create cpuIntensiveApp.c

---
+++ cpuIntensiveApp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double i = 0;
+    double sum = 0;
+
+    while(1){
+        sum = sum + i;
+        i = 3.14 * i;
+    }
+    return 0;
+}
